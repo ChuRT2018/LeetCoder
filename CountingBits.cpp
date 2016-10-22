adcbf648db50fc3382cd344d8748141ffd7c09b2
blacker <1755654916@qq.com>
2016-10-22 20:55:12 +0800

    Update and rename CountintBits.cpp to CountingBits.cpp

--- CountingBits.cpp
+++ CountingBits.cpp
@@ -1,9 +1,8 @@
-/* ²Î¿¼ÌÖÂÛ */
 class Solution {
 public:
     vector<int> countBits(int num) {
         vector<int> ans(num + 1,0);
-        for(int i = 0; i < num + 1 ; i++){
+        for(int i = 1; i <= num; i++){
             ans[i] = ans[i & (i - 1)] + 1;
         }
         return ans;
